f67121e4fec166be3e9281ad5ec21542b3c7ac46
Woron Mene <meneworon4@gmail.com>
2022-10-02 22:03:23 +0100

    build error test4.0

--- src/pages/ContactPage/index.js
+++
@@ -1,2 +0,0 @@
-export { default as ContactMod } from './Contact'
-
--- src/pages/ProductsPage/index.js
+++
@@ -1,5 +0,0 @@
-export { default as ProductLayout1 } from "./layout1/Layout1";
-export { default as ProductLayout2 } from "./layout2/Layout2";
-export { default as JustHeader } from "./justheader/JustHeader";
-export { default as WhatElse} from './whatElse/WhatElse';
-export { default as HavePlatform} from './havePlatform/HavePlatform'
